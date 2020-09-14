28d11a1e326b828f502b6e84a9b9b48c60bb432a
DriesDD <driesdedecker@gmail.com>
2020-09-14 15:43:33 +0200

    refactor and clean up everything

--- pz.php
+++ pz.php
@@ -1,77 +1,60 @@
 <?php
 
 function orderPizza($pizzaType, $Customer)
-{   echo 'Creating new order... <br>';
-    echo 'A ' . $pizzaType;
-    $Price = calc_cts($pizzaType);
+{   $Customer = ucfirst($Customer);
+    $Address = getAddress($Customer);
+    $Price = getPrice($pizzaType);
 
-    $address = 'unknown';
-    switch ($Customer) {
-        case 'koen': $address = 'a yacht in Antwerp';
-            break;   
-        case 'manuele': $address = 'somewhere in Belgium';
-            break;  
-        case 'students': $address = 'BeCode office';
-            break;      
-        default: $address = 'unknown';
-            break;
-    }
-
-    echo ' pizza should be sent to ' . $Customer . ". <br>The address: {$address}.";
-    echo '<br>';
-    echo 'The bill is €' . $Price . '.<br>';
+    echo 'Creating new order...<br>';
+    echo "A {$pizzaType} pizza should be sent to {$Customer}.<br>";
+    echo "The address: {$Address}.<br>";
+    echo "The bill is €{$Price}.<br>";
     echo "Order finished.<br><br>";
 }
 
-function total_price($Price)
+
+function getAddress($Customer)
 {
+    switch ($Customer) {
+        case 'Koen':
+            return 'a yacht in Antwerp';
+            break;
+        case 'Manuele':
+            return 'somewhere in Belgium';
+            break;
+        case 'Students':
+            return 'BeCode office';
+            break;
+        default:
+            return 'unknown';
+            break;
+    }
+}
+
+function getPrice($pizzaType)
+{
+    switch ($pizzaType) {
+        case 'marguerita':
+            $Price = '5';
+            break;
+        case 'golden':
+            $Price = '100';
+            break;
+        case 'calzone':
+            $Price = '10';
+            break;
+        case 'hawaii':
+            throw new Exception('Computer says no');
+            break;
+        default:
+            $Price = 'unknown';
+            break;
+    }
     return $Price;
 }
 
-function test($p_type)
-{
-    echo "Test: type is {$p_type}. <br>";
-}
-
-function calc_cts($p_type)
-{
-    $cst = 'unknown';
-
-    if ($p_type == 'marguerita') {
-        $cst = 5;
-    } else {
-        if ($p_type == 'golden') {
-            $cst = 100;
-        }
-
-        if ($p_type == 'calzone') {
-            $cst = 10;
-        }
-
-        if ($p_type == 'hawai') {
-            throw new Exception('Computer says no');
-        }
-    }
-
-    return $cst;
-}
-
-function ordr_piz_all()
-{
-    $test = 0;
+(function () {
     orderPizza('calzone', 'koen');
     orderPizza('marguerita', 'manuele');
-
     orderPizza('golden', 'students');
-}
-
-function make_Allhappy($do_it)
-{
-    if ($do_it) {
-        ordr_piz_all();
-    } else {
-        // Should not do anything when false
-    }
-}
-
-make_Allhappy(true);
+})();
